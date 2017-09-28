5927f653106bcfcee08dde031c059c31323aa8f2
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-28 17:14:08 -0400

    Added conditionals to rendering and started fetching to backend api

--- src/App.js
+++ src/App.js
@@ -5,15 +5,57 @@ import WeatherContainer from './containers/WeatherContainer';
 
 class App extends Component {
 
-// this needs to have call to our own backend 
+// this needs to have call to our own backend
 // so as to pass shouldRender down to each container?
+  constructor(){
+    super()
+
+    this.state = {
+      zipcode: "",
+      news: null,
+      weather: null
+    }
+  }
+
+  componentDidMount(){
+    // had to comment out all the authorization stuff on the backend
+    // it was just getting a redirect to '/welcome' when I did a fetch and that route didn't exist.
+    // I put two users in the database to test whether I could set state based on which user was hit.
+    // comment out one and uncomment the other to see how it handles it.
+    // it's connecting to the backend api we just need to fix the authorization issue.
+    // I don't think it should be anything too crazy
+    // Probably should look at adding routes.
+    // An index page would be good with option to 'sign up' && 'sign in'
+
+    fetch('http://localhost:3000/api/v1/users/1')
+    .then((response) => response.json())
+    .then((newResponse) => {
+      this.setState({
+        zipcode: newResponse.zipcode,
+        news: newResponse.news,
+        weather: newResponse.weather
+      })
+    })
+  }
+
+  //   fetch('http://localhost:3000/api/v1/users/2')
+  //   .then((response) => response.json())
+  //   .then((newResponse) => {
+  //     this.setState({
+  //       zipcode: newResponse.zipcode,
+  //       news: newResponse.news,
+  //       weather: newResponse.weather
+  //     })
+  //   })
+  // }
 
   render() {
+    console.log("this.state: ", this.state)
     return (
       <div className="App ui right aligned grid">
         <p>You're in the app</p>
-        <NewsListContainer />
-        <WeatherContainer />
+        {this.state.news ? <NewsListContainer />: null}
+        {this.state.weather ? <WeatherContainer />: null}
       </div>
     );
   }
