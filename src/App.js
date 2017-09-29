7c317c84e1a3cd5edd1a31ed6c7da1e943f9e9a6
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-09-29 10:56:12 -0400

    did some routing stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import NewsListContainer from './containers/NewsListContainer';
 import WeatherContainer from './containers/WeatherContainer';
+import { BrowserRouter as Router} from 'react-router-dom'
+import {Route} from 'react-router-dom'
+import LogIn from './components/LogIn'
 
 
 class App extends Component {
@@ -11,11 +14,36 @@ class App extends Component {
     super()
 
     this.state = {
-      zipcode: "",
-      news: null,
-      weather: null
+      user: null
     }
   }
+
+  // logIn = (event) => {
+  //
+  // }
+
+  logInUser = (logInParams) => {
+    fetch("http://localhost:3000/login", {
+      method: 'post',
+      body: JSON.stringify(logInParams),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((respJSON) => {
+      console.log(respJSON)
+      localStorage.setItem('jwtToken', respJSON.jwt)
+      this.setState({
+        user: respJSON.user
+      })
+    })
+  }
+
+
 
   componentDidMount(){
     // had to comment out all the authorization stuff on the backend
@@ -28,15 +56,16 @@ class App extends Component {
     // Probably should look at adding routes.
     // An index page would be good with option to 'sign up' && 'sign in'
 
-    fetch('http://localhost:3000/api/v1/users/1')
-    .then((response) => response.json())
-    .then((newResponse) => {
-      this.setState({
-        zipcode: newResponse.zipcode,
-        news: newResponse.news,
-        weather: newResponse.weather
-      })
-    })
+    // fetch('http://localhost:3000/api/v1/users/1')
+    // .then((response) => response.json())
+    // .then((newResponse) => {
+    //   this.setState({
+    //     id: newResponse.id,
+    //     zipcode: newResponse.zipcode,
+    //     news: newResponse.news,
+    //     weather: newResponse.weather
+    //   })
+    // })
   }
 
   //   fetch('http://localhost:3000/api/v1/users/2')
@@ -54,9 +83,12 @@ class App extends Component {
     console.log("this.state: ", this.state)
     return (
       <div className="App ui right aligned grid">
+        <Router>
+          <Route path='/login' render={() => <LogIn logInUser={this.logInUser} />} />
+        </Router>
         <p>You're in the app</p>
-        {this.state.news ? <NewsListContainer />: null}
-        {this.state.weather ? <WeatherContainer />: null}
+        {this.state.user ? <NewsListContainer />: null}
+        {this.state.user ? <WeatherContainer />: null}
       </div>
     );
   }
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,19 +1,56 @@
 import React from 'react';
 
-const LogIn = (props) => {
+class LogIn extends React.Component {
 
-	// onsubmit={}
+	constructor(){
+		super()
 
-	handleSubmit = () => {
-		//props.callback(some info from form)
+		this.state = {
+			username: "",
+			password: ""
+		}
 	}
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<label>username</label>
-			<input type="text"/>
-			<label>password</label>
-			<input type="password"/>
-		</form>
-	);
+	handleUsernameChange = (event) => {
+		this.setState({
+			username: event.target.value
+		})
+	}
+
+	handlePasswordChange = (event) =>{
+		this.setState({
+			password: event.target.value
+		})
+	}
+
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		console.log("Something happened")
+		console.log(this.state)
+		//props.callback(some info from form)
+		// log in user
+		console.log(this.props)
+		this.props.logInUser({username: this.state.username, password: this.state.password})
+
+		this.setState({
+			username: "",
+			password: ""
+		})
+
+	}
+
+	render(){
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>username</label>
+				<input type="text" onChange={this.handleUsernameChange} value={this.state.username}/>
+				<label>password</label>
+				<input type="password" onChange={this.handlePasswordChange} value={this.state.password}/>
+				<input type="submit" />
+			</form>
+		);
+	}
 }
+
+export default LogIn
